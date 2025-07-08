0ae3a31c087121948a621bb6156c36dfd94b3b49
sgunwar <59456444+sgunwar@users.noreply.github.com>
2025-07-08 11:51:34 -0600

    Update qu_2_answer.sh

--- touhidece/qu_2_answer.sh
+++ touhidece/qu_2_answer.sh
@@ -6,5 +6,5 @@ groupadd admin
 chown -R .admin admins/
 chmod -R 760 admins/
 
-#Second Part of the question is solved by enabling Sticky bit upon the G "group"
+#Second Part of the question is solved by enabling setGID upon the G "group"
 chmod -R g+s admins/
