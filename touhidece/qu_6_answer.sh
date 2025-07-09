da3277cfe20c166d36845196c1f771b3366f013f
sgunwar <59456444+sgunwar@users.noreply.github.com>
2025-07-09 06:52:54 -0600

    Update qu_6_answer.sh

--- touhidece/qu_6_answer.sh
+++ touhidece/qu_6_answer.sh
@@ -9,3 +9,17 @@ systemctl enable --now httpd
 	ServerName station1.marry.com
 	DocumentRoot /var/www/marry.com
 <VirtualHost/>
+
+
+======================================================================================================
+# yum install -y httpd
+# chkconfig httpd on
+# cd /var/www/html
+# wget http://ip/dir/example.html
+# cp example.com index.html
+# vim /etc/httpd/conf/httpd.conf
+NameVirtualHost 192.168.0.254:80
+<VirtualHost 192.168.0.254:80>
+DocumentRoot /var/www/html/
+ServerName station.domain40.example.com
+</VirtualHost>
